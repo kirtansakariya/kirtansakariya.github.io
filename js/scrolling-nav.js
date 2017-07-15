5e470172940d41a2336c67e6f07ce8fa4d285fb0
Kirtan <kirtansakariya@gmail.com>
2017-07-15 16:52:11 -0400

    added more id vars to js file

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -27,3 +27,5 @@ var hobbies = document.getelementyById("hobbies");
 var goals = document.getElementById("goals");
 var contact = document.getElementById("contact");
 var bottom = document.getElementById("bottom");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
