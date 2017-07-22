69f55fe33930565c1e65c19ef52437d3da088d59
Kirtan <kirtansakariya@gmail.com>
2017-07-22 18:19:06 -0400

    added new js file for theme change

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -17,19 +17,3 @@ $(function() {
         event.preventDefault();
     });
 });
-
-//JavaScript to help with theme change
-var intro = document.getElementById("intro");
-var about = document.getElementById("about");
-var certificates = document.getElementById("certificates");
-var hobbies = document.getelementyById("hobbies");
-var goals = document.getElementById("goals");
-var contact = document.getElementById("contact");
-var bottom = document.getElementById("bottom");
-var button1 = document.getElementById("button1");
-var button2 = document.getElementById("button2");
-button1.addEventListener;
-button2.addEventListener;
-button1.onclick = alert("hello");
-
-//HANDLE WITH ONLOAD MAYBE?
