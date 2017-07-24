0261dfb0b27582630a6aa1be1f2ac2ed818c7c44
Kirtan <kirtansakariya@gmail.com>
2017-07-23 23:52:12 -0500

    added new theme.js file

---
+++ js/theme.js
@@ -0,0 +1,15 @@
+//JavaScript to help with theme change
+var intro = document.getElementById("intro");
+var about = document.getElementById("about");
+var certificates = document.getElementById("certificates");
+var hobbies = document.getelementyById("hobbies");
+var goals = document.getElementById("goals");
+var contact = document.getElementById("contact");
+var bottom = document.getElementById("bottom");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+button1.addEventListener;
+button2.addEventListener;
+button1.onclick = function(){
+    alert("hello");
+}
