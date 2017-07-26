93f80d60bcc61a94ccfeb3e42b9f7ce8d5fd9242
Kirtan <kirtansakariya@gmail.com>
2017-07-26 08:22:40 -0400

    updated theme.js file

--- js/theme.js
+++ js/theme.js
@@ -1,14 +1,12 @@
 //JavaScript to help with theme change
-window.onload = window.alert("hi");
-var intro = document.getElementById("intro");
-var about = document.getElementById("about");
-var certificates = document.getElementById("certificates");
-var hobbies = document.getelementyById("hobbies");
-var goals = document.getElementById("goals");
-var contact = document.getElementById("contact");
-var bottom = document.getElementById("bottom");
-var button1 = document.getElementById("button1");
-var button2 = document.getElementById("button2");
-button1.onclick = function () {
-    window.alert("hello");
+window.onload = function () {
+    var intro = document.getElementById("intro");
+    var about = document.getElementById("about");
+    var certificates = document.getElementById("certificates");
+    var hobbies = document.getelementyById("hobbies");
+    var goals = document.getElementById("goals");
+    var contact = document.getElementById("contact");
+    var bottom = document.getElementById("bottom");
+    var button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
 };
