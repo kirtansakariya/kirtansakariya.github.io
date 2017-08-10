9d049efc48d344e9f79d10dc17f78516f149cea3
Kirtan <kirtansakariya@gmail.com>
2017-08-10 08:39:27 -0400

    updated button/navigation in js file

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,7 @@
 //JavaScript to help with theme change
 window.onload = function () {
-    var buttons = document.getElementById("button");
-    var navigation = document.getElementyById("navigation");
+    var buttons = document.getElementsByClassName("button");
+    var navigation = document.getElementsByClassName("navigation");
     var intro = document.getElementById("intro");
     var about = document.getElementById("about");
     var certificates = document.getElementById("certificates");
@@ -19,13 +19,12 @@ dark.addEventListener("click", function () {
 });
 light.addEventListener("click", function () {
     window.alert("light");
-    buttons.style.background = "#e3dfe8";
-    navigation.style.background = "#dd8247";
     intro.style.background = "#7cc6ff";
     about.style.background = "#76e7cd";
     certificates.style.background = "#7cc6ff";
     hobbies.style.background = "#76e7cd";
     goals.style.background = "#7cc6ff";
     contact.style.background = "#76e7cd";
+    buttons.style.background = "#e3dfe8";
     bottom.style.background = "#dd8247";
 });
